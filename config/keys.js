629056467aeed3ddd404cba5876f4c6b7b9fbeed
aadi <aadiyaara@gmail.com>
2019-02-22 22:56:29 +0530

    Heroku Deploy Phase-8 Complete

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+    google: {
+
+    },
+    linkedIn: {
+
+    },
+    mongoDB: {
+        dbURI: 'mongodb://kuluser:kuluser1@eb7-shard-00-00-vjvyi.mongodb.net:27017,eb7-shard-00-01-vjvyi.mongodb.net:27017,eb7-shard-00-02-vjvyi.mongodb.net:27017/test?ssl=true&replicaSet=EB7-shard-0&authSource=admin&retryWrites=true'
+    }
+}
